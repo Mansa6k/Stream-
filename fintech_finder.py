c1a3f5d09f31bd921e7ddecd92d59637ed403e88
Mansa6k <118853744+Mansa6k@users.noreply.github.com>
2023-04-20 09:57:27 -0400

    Update fintech_finder.py

--- fintech_finder.py
+++ fintech_finder.py
@@ -32,9 +32,6 @@ from web3 import Web3
 ganache_url = "http://127.0.0.1:7545"
 w3 = Web3(Web3.HTTPProvider(ganache_url))
 
-# Check if connected to Ethereum provider
-if not w3.isConnected():
-    raise Exception("Not connected to Ethereum provider")
 #w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 ################################################################################
 # Step 1:
